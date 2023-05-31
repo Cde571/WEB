012ddb3d3f348496a706f9b580ae901d2f235b11
Cde571 <cristian.echeverry1@udea.edu.co>
2023-05-31 17:59:40 -0500

    web page

---
+++ src/main/java/config/Conexion.java
@@ -0,0 +1,30 @@
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author cde57
+ */
+public class Conexion {
+    
+    Connection conexion = null;
+
+    String base = "almacen"; //Nombre de la base de datos
+    String url = "jdbc:mysql://localhost:3306/" + base; //Direccion, puerto y nombre de la Base de Datos
+    String user = "root"; //Usuario de Acceso a MySQL
+    String password = "Cristianego57"; //Password del usuario
+
+    public Connection getConexion() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexion = DriverManager.getConnection(url, user, password);
+            return conexion;
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e.toString());
+            return null;
+        }
+    }
+}
